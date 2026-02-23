ef3074cccd3bd0c2e7498b5f2121e4c37660063e
svijayasree47-jpg <s.vijayasree47@gmail.com>
2026-02-23 10:35:48 +0530

    vijayasree added UC2-HardCodePalidrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String s;
         System.out.println("Welcome to Palindrome Checker App Management System");
         System.out.println("Enter a string(lower case):");
-        s=sc.nextLine();
-        String text="";
-        for(int i=s.length()-1; i>=0; i--){
-            text=text + s.charAt(i);
+        s = sc.nextLine();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
         }
-        if (s.equals(text)) {
+
+        if (isPalindrome) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
         }
-
-        sc.close();
     }
 }
-    }
 
 
 
